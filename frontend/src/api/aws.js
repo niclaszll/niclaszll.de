1685e328d242c86f9e706f9f639be7d8ba2c1ed8
Niclas <zellpublic+github@icloud.com>
2021-04-12 21:50:14 +0200

    test headers

--- frontend/src/api/aws.js
+++ frontend/src/api/aws.js
@@ -1,9 +1,5 @@
 export function getVisitorCount() {
   return fetch(`${process.env.GATSBY_API_ENDPOINT}/count`, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
     method: 'GET',
   }).then((res) => res.json()).catch(err => console.log(err))
 }
